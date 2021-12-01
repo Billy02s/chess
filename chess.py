c9207e21e259f2756cd8677c638ab323d425a4b8
Bill <bill.lee2002s@gmail.com>
2021-12-01 17:21:13 +0000

    initial commit

---
+++ chess.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import*
+
+pygame.init()
+screen_width = 800
+screen_height = 800
+screen = pygame.display.set_mode([screen_width,screen_height])
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+

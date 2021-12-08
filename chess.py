2d25969e4d6e30edd256543223de0f3171710021
Bill <bill.lee2002s@gmail.com>
2021-12-08 22:18:25 +0000

    created chess board

--- chess.py
+++ chess.py
@@ -2,34 +2,51 @@ import pygame
 import os
 
 pygame.init()
-width,height = 1000, 900
-screen = pygame.display.set_mode((width,height))
+width,height = 800, 800
+board = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Chess")
 pygame.display.update()
 
 piece_width, piece_height = 100, 100
 
-wpawn_image = pygame.image.load('/home/billy/assets/wPawn.png')
+
+wpawn_image = pygame.image.load('assets/wPawn.png')
 wpawn = pygame.transform.scale(wpawn_image, (piece_width, piece_height))
-bpawn_image = pygame.image.load('/home/billy/assets/bPawn.png')
+bpawn_image = pygame.image.load('assets/bPawn.png')
 bpawn = pygame.transform.scale(bpawn_image, (piece_width, piece_height))
 
-
-background = (150,150,150)
+black = (112, 79, 54)
+white = (199, 153, 117)
 
 fps = 60
 
-def draw_window():
-    screen.fill(background)
-    screen.blit(wpawn,(200, 790))
-    screen.blit(bpawn,(200,0))
+def draw_board():
+    tile = 100
+    board.fill(white)
+
+    count = 2
+
+    for i in range(0,8):
+        for j in range(0,8):
+            if count % 2 == 0:
+                pygame.draw.rect(board, white, [tile*j, tile*i, tile, tile])
+            else:
+                pygame.draw.rect(board, black, [tile*j, tile*i, tile, tile])
+            count += 1
+        count -= 1
+
+    pygame.draw.rect(board, black, [tile,tile,8*tile,8*tile],1)
+    
+
+    board.blit(wpawn,(200, 590))
+    board.blit(bpawn,(200,0))
     pygame.display.update()
 
 
 def main():
     clock = pygame.time.Clock()
     run = True
-    draw_window()
+    draw_board()
     
     while run:
         clock.tick(fps)

85a7506537060bf09ed1b0e45c32a2f4b561583c
Bill <bill.lee2002s@gmail.com>
2021-12-08 15:11:16 +0000

    added assets

--- chess.py
+++ chess.py
@@ -1,14 +1,44 @@
 import pygame
-from pygame.locals import*
+import os
 
 pygame.init()
-screen_width = 800
-screen_height = 800
-screen = pygame.display.set_mode([screen_width,screen_height])
+width,height = 1000, 900
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("Chess")
 pygame.display.update()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+piece_width, piece_height = 100, 100
+
+wpawn_image = pygame.image.load('/home/billy/assets/wPawn.png')
+wpawn = pygame.transform.scale(wpawn_image, (piece_width, piece_height))
+bpawn_image = pygame.image.load('/home/billy/assets/bPawn.png')
+bpawn = pygame.transform.scale(bpawn_image, (piece_width, piece_height))
+
+
+background = (150,150,150)
+
+fps = 60
+
+def draw_window():
+    screen.fill(background)
+    screen.blit(wpawn,(200, 790))
+    screen.blit(bpawn,(200,0))
+    pygame.display.update()
+
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    draw_window()
+    
+    while run:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
 
